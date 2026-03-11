7db9b84958430130fd9f7ca2fde4c0f53d664110
Maksym Holubchenko <152689555+AideNaDa@users.noreply.github.com>
2026-03-11 20:29:31 +0100

    add functionality for notes and JSON saves and worked a little with cli.py

--- cli.py
+++ cli.py
@@ -8,8 +8,47 @@ def parse_input(user_input):
     cmd = cmd.strip().lower()
     return cmd, *args
 
+# вспомагательная функция вывода инструкции
+def display_instruction() -> str:
+    help_text = f"""
+{'='*100}
+            ASSISTANT BOT COMMAND GUIDE  
+{'='*100}
 
-def run_cli(address_book, note_book):
+  CONTACT MANAGEMENT:
+  -------------------
+  add [name] <ph> <bd> <addr> <em>  - Create new contact (order doesn't matter)
+  add [name] [phone/email]          - Add extra phone or email to contact
+  birthdays [days]                  - Shows upcoming birthdays in 'X' days
+  del [name]                        - Delete contact completely
+  del [name] [field]                - Delete specific phone, email, bd or addr
+  edit [name] [old] [new]           - Replace phone/email or update bd/addr
+  find [query]                      - Search contacts by any field
+  show-all                          - Display all saved contacts
+
+  NOTE MANAGEMENT:
+  ----------------
+  add-note [title] "[text]" <tags>  - Create a note (text in quotes "")
+  add-tag [title] [tag1] [tag2]...  - Add one or more tags to a note
+  del-note [title]                  - Delete note completely
+  del-note [title] [tag]            - Delete specific tag from a note
+  edit-note [title] [new_text]      - Update note text
+  edit-note [title] [old_t] [new_t] - Replace an old tag with a new one
+  find-note [query]                 - Search notes by title or tags
+  show-all-note                     - Display all saved notes
+
+  GENERAL:
+  --------
+  hello                             - Greeting from bot
+  close / exit                      - Save data and exit program
+  ? / help                          - Show this manual
+{'='*100}
+    """
+    return help_text
+
+
+def run_cli(address_book, notebook):
+    address_book, notebook = load_data()
     print("Assistant bot started. Type 'exit' to quit.")
     while True:
         try:
@@ -17,10 +56,33 @@ def run_cli(address_book, note_book):
         except KeyboardInterrupt:
             print("\nGood bye!")
             break
-        command, args = parse_input(user_input)
-        if command in ["close", "exit"]:
-            print("Good bye!")
-            save_data(...)
-            break
-        elif command == "hello":
-            print("How can I help you?")
+        command, *args = parse_input(user_input)
+        match command:
+            case "close":
+                print("Good bye!")
+                save_data(address_book, notebook)
+                break
+            case "exit":
+                print("Good bye!")
+                save_data(address_book, notebook)
+                break
+            case "hello":
+                print(
+                    "Hello, enter '?' or 'help' for instruction"
+                )
+            case 'help':
+
+
+            # notebook
+            case "add-note":
+                print(add_note(args, notebook))
+            case "add-tag":
+                print(add_tag(args, notebook))
+            case "del-note":
+                print(del_note(args, notebook))
+            case "edit-note":
+                print(edit_note(args, notebook))
+            case "find-note":
+                print(find_note(args, notebook))
+            case "show-all-note":
+                print(show_all_notes(notebook))
--- storage.py
+++ storage.py
@@ -1,6 +1,47 @@
-def save_data():
-    pass
+from models.note_book import NoteBook, Note
+from models.address_book import AddressBook, Record
+import json
+
+FILENAME = "data.json"
 
 
-def load_data():
-    pass
+def save_data(address_book: AddressBook, notebook: NoteBook):
+    combined_data = {
+        "contacts": {
+            name: record.to_dict()
+            for name, record in address_book.data.items()
+        },
+        "notes": {
+            title: note.to_dict() for title, note in notebook.data.items()
+        },
+    }
+
+    with open(FILENAME, "w", encoding="utf-8") as f:
+        json.dump(combined_data, f, ensure_ascii=False, indent=2)
+
+
+def load_data() -> tuple[AddressBook, NoteBook]:
+    notebook = NoteBook()
+    address_book = AddressBook()
+
+    try:
+        with open(FILENAME, "r", encoding="utf-8") as f:
+            full_data = json.load(f)
+
+            # Uploading contacts
+            contacts = full_data.get("contacts", {})
+            for record_data in contacts.values():
+                address_book.add_record(
+                    address_book.add_record(Record.from_dict(record_data))
+                )
+
+            # Uploading notes
+            notes = full_data.get("notes", {})
+            for note_data in notes.values():
+                notebook.add_note(Note.from_dict(note_data))
+
+    except (FileNotFoundError, json.JSONDecodeError):
+        # If the file is missing or empty/corrupted
+        print("The data file was not found or is empty.")
+
+    return address_book, notebook
